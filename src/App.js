79aee6c8d37ecb0fce48d0d501def3511b6693be
Kiko <sthyeon11#gmail.com>
2023-03-11 15:53:09 +0000

    apply-contextAPI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from './logo.svg';
 import './App.scss';
-import InputSample from './InputSample';
 import CreateUser from './components/CreateUser';
 import UserList from './components/UserList';
-import { useEffect, useRef, useState, useMemo, useCallback, useReducer} from 'react';
-import useInputs from './hooks/useInputs';
+import React, { useEffect, useRef, useState, useMemo, useCallback, useReducer} from 'react';
 
 const countActiveUsers=(users)=>{
   console.log('활성 사용자 수 세는중')
@@ -60,54 +58,23 @@ const reducer=(state, {type, user, id})=>{
   }
 }
 
+//context api 기본 
+export const UserDispatch =React.createContext(null);
+
 
 function App() {
 
-  const [{username, email}, onChange, reset] =useInputs({
-    username : '',
-    email : ''
-  })
 
   const [state, dispatch] = useReducer(reducer, initialState)
-  const nextId = useRef(4);
   const {users} = state;
   const count = useMemo(()=>countActiveUsers(users),[users]);
 
-
-
-  const onCreate = useCallback(()=>{
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email,
-      }
-    })
-    reset();
-    nextId.current++;
-  },[username,email]);
-
-  const onToggle = useCallback((id)=>{
-    dispatch({
-      type: "TOGGLE_USER",
-      id
-    })
-  },[])
-
-  const onRemove = useCallback((id)=>{
-    dispatch({
-      type: "REMOVE_USER",
-      id
-    })
-  },[])
-      
   return (
-    <>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}></CreateUser>
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}></UserList>
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser></CreateUser>
+      <UserList users={users} ></UserList>
       <div>활성 사용자 수 : {count}</div>
-    </>
+    </UserDispatch.Provider>
 
   );
 }
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useRef, useContext } from 'react';
+import { UserDispatch } from '../App';
+import useInputs from '../hooks/useInputs';
 
 
 //onCreate 에 쓰일 인자들마저 props 로 받기
-const CreateUser=({username, email, onChange, onCreate})=>{
+const CreateUser=()=>{
+    const dispatch = useContext(UserDispatch)
+    const [{username, email}, onChange, reset] = useInputs({
+        username: '',
+        email: ''
+    });
+    const nextId = useRef(4);
+
+    const onCreate=()=>{
+        dispatch({
+            type: 'CREATE_USER',
+            user : {
+                id:nextId.current,
+                username,
+                email
+            }
+        })
+        reset();
+        nextId.current++;
+    }
+
     return(
         <div>
-            <input type="text" name="username" placeholder='계정명' onChange={onChange
-            } value={username} />
-            <input type="text" name="email" placeholder='이메일' onChange={onChange
-            } value={email} />
+            <input type="text" name="username" placeholder='계정명' onChange={onChange} value={username} />
+            <input type="text" name="email" placeholder='이메일' onChange={onChange} value={email} />
             <button onClick={onCreate}>등록</button>
         </div>
-        
     )
 }
 
